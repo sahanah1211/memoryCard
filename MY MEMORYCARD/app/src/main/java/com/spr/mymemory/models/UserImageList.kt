e8031dd600ad89d5df82e532765c9db7ae4ff715
sahanah1211 <79031014+sahanah1211@users.noreply.github.com>
2022-02-16 23:53:31 +0800

    Release V1.0
    Features:
    ★ Choose from various board sizes: 4 x 2, 6 x 3, and 6 x 4
    ★ Play with colorful icons in the default mode
    ★ Customize the game with pictures on your phone, and then share your personalized game with other friends/family who have the app - they can play your game!
    ★ Easily track the number of moves you’ve made and the number of pairs you’ve found.

---
+++ MY MEMORYCARD/app/src/main/java/com/spr/mymemory/models/UserImageList.kt
@@ -0,0 +1,9 @@
+package com.spr.mymemory.models
+
+import com.google.firebase.firestore.PropertyName
+import com.google.j2objc.annotations.Property
+
+data class UserImageList (
+    @PropertyName("images") val images: List<String>? = null
+
+)
